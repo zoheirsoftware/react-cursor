781c01a712b8346ba3a107671bbe267702eccb23
zoheirsoftware <zoheir.s@yahoo.com>
2026-06-23 07:12:33 +0330

    part-22

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from "react";
 import TopForm from "./TopForm";
 import TaskItem from "./TaskItem";
- 
- 
- 
-const App = () => {
- 
+import {taskContext} from "./TestContext"
 
- 
+const App = () => {
+ const [itemtask, setItemTask] = useState([
+    { id: 1, titel: "تمرین شماره 1",done:false },
+    { id: 1, titel: "تمرین شماره 2",done:true },
+    { id: 1, titel: "تمرین شماره 3" ,done:false}]);
+
   return (
     <>
-     <div className="container w-100 h-100 p-3">
-            <div className="row h-100 justify-content-center align-align-items-start">
-                <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-                    <TopForm />
-                     <TaskItem/>
-                </div>
-            </div>
+      <div className="container w-100 h-100 p-3">
+        <div className="row h-100 justify-content-center align-align-items-start">
+          <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
+            <taskContext.Provider value={{itemtask, setItemTask}} >
+            <TopForm />
+            <TaskItem />
+            </taskContext.Provider>
+           
+          </div>
         </div>
-      
+      </div>
     </>
   );
 };
--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -1,26 +1,36 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import {taskContext} from "./TestContext"
 
 const TaskItem =()=>{
+    // const item=useContext(taskContext)
+    // console.log(item.itemtask)
+    const {itemtask, setItemTask}=useContext(taskContext)
+
     return (
         <>
          <ul className="list-group m-0 p-0 mt-2">
-            <li className="list-group-item d-flex justify-content-between">
-                کار شماره 1
-                <span>
-                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                </span>
-            </li>
-
-            <li className="list-group-item d-flex justify-content-between list-group-item-success">
-                کار شماره 2
-                <span>
-                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                    <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                </span>
-            </li>
+            {
+                itemtask.map(t=>(
+                    <li className={`list-group-item d-flex justify-content-between 
+                    ${t.done?"list-group-item-success":""}`}>
+                   {t.titel}
+                    <span>
+                        {
+                            t.done?(  <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>):
+                            (
+                                <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
+                               
+                            )
+                        }
+                      
+                       
+                        <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+                    </span>
+                </li>
+                ))
+            }
+         
+ 
 
         </ul>
         </>
