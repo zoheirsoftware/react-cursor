e6d0c7bcc174cd591f96c565b98a6b255425f32e
zoheirsoftware <zoheir.s@yahoo.com>
2026-08-05 16:04:33 +0330

    part-49

--- src/todos/Counter.jsx
+++ src/todos/Counter.jsx
@@ -1,54 +1,43 @@
 import React, { useReducer, useState } from "react";
 
-const init = 0;
+const init = {
+  value1: 0,
+  value2: 1,
+};
 const reduser = (state, action) => {
-  //   return state + action;
-//   if (action === "increser") {
-//     return state + 1;
-//   }
-
-//   if (action === "decreser") {
-//     return state - 1;
-//   }
-//   if (action === "resat") {
-//     return init;
-//   }
-
-  switch (action) {
+  switch (action.type) {
     case "increser":
-      return state + 1; 
+      return { ...state, value1: state.value1 + action.val };
     case "decreser":
-      return state - 1;
-     
+      return { ...state, value1: state.value1 - action.val };
+      case "increser2":
+        return { ...state, value2: state.value2 + action.val };
+      case "decreser2":
+        return { ...state, value2: state.value2 - action.val };
     case "resat":
       return init;
-      
+    default:
+      return state;
   }
 };
 
 export const Counter = () => {
-  // const [count,setCount]=useState(0)
-  const [count, dispatch] = useReducer(reduser, 0);
+  const [count, dispatch] = useReducer(reduser, init);
 
-  const handelcounter = () => {
-    // setCount(count + 1)
-    // dispatch(1)
-  };
   return (
     <div className="text-center mt-3">
-      <h3 className="text-info  text-center">{count}</h3>
-      {/* <button  className="btn btn-success"   onClick={ handelcounter}  > */}
-      {/* <button  className="btn btn-success"   onClick={ handelcounter}  > */}
-      <button className="btn btn-success" onClick={() => dispatch("increser")}>
-        افزایش
-      </button>
-      <button
-        className="btn btn-danger mr-3"
-        onClick={() => dispatch("decreser")}
-      >
-        کاهش
-      </button>
-      <button className="btn btn-info mr-3" onClick={() => dispatch("resat")}>
+      <h3 className="text-info  text-center">{count.value1}</h3>
+      <h3 className="text-info  text-center">{count.value2}</h3>
+
+      <div className="text-center">
+        <button className="btn btn-success"  onClick={() => dispatch({type:"increser",val:1})}>   افزایش  </button>
+        <button className="btn btn-danger mr-3"  onClick={() => dispatch({type:"decreser",val:2})}  >  کاهش  </button>
+      </div>
+      <div className="text-center mt-2">
+        <button  className="btn btn-success"  onClick={() => dispatch({type:"increser2",val:5})}  > افزایش 2 </button>
+        <button className="btn btn-danger mr-3"   onClick={() => dispatch({type:"decreser2",val:3})}   >   کاهش 2  </button>
+      </div>
+      <button className="btn btn-info mr-3" onClick={() => dispatch({type:"resat"})}>
         ریسیت
       </button>
     </div>
