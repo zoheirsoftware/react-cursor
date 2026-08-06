3d832a3932924000418c51b88e1d141463ea3ef5
zoheirsoftware <zoheir.s@yahoo.com>
2026-08-06 15:53:51 +0330

    part-53

--- src/todos/Counter.jsx
+++ src/todos/Counter.jsx
@@ -1,45 +1,45 @@
-import React, { useReducer, useState } from "react";
-
-const init = {
-  value1: 0,
-  value2: 1,
-};
-const reduser = (state, action) => {
-  switch (action.type) {
-    case "increser":
-      return { ...state, value1: state.value1 + action.val };
-    case "decreser":
-      return { ...state, value1: state.value1 - action.val };
-      case "increser2":
-        return { ...state, value2: state.value2 + action.val };
-      case "decreser2":
-        return { ...state, value2: state.value2 - action.val };
-    case "resat":
-      return init;
-    default:
-      return state;
-  }
-};
+import React, { useMemo, useReducer, useState } from "react";
 
 export const Counter = () => {
-  const [count, dispatch] = useReducer(reduser, init);
+  const [count, setCount] = useState(0);
+  const [countTwo, setCountTwo] = useState(0);
+  const incream = () => {
+    setCount(count + 1);
+  };
+  const increamTwo = () => {
+    setCountTwo(countTwo + 1);
+  };
+
+  //با هر بار تغییر state این تابع اجرا میشود
+  // const isEvn=()=>{
+  //   return count % 2===0
+  // }
+  
+  //اما با استفاده useMemo هنگام تغییر stste => count اجرا میشود
+  const isEvn=useMemo(()=>{
+    return count % 2===0
+  },[count])
 
   return (
     <div className="text-center mt-3">
-      <h3 className="text-info  text-center">{count.value1}</h3>
-      <h3 className="text-info  text-center">{count.value2}</h3>
-
+    {/* <h3 className="text-center">{isEvn()?"زوج":"فرد"}</h3> */}
+    <h3 className="text-center">{isEvn?"زوج":"فرد"}</h3>
       <div className="text-center">
-        <button className="btn btn-success"  onClick={() => dispatch({type:"increser",val:1})}>   افزایش  </button>
-        <button className="btn btn-danger mr-3"  onClick={() => dispatch({type:"decreser",val:2})}  >  کاهش  </button>
+        <button
+          className="btn btn-success"
+          onClick={incream}
+        >
+        {`count - one :${count}`}
+        </button>
+        <br/>  <br/>
+        <button
+          className="btn btn-danger mr-3"
+          onClick={increamTwo}
+        >
+          {`count - Two  :${countTwo }`}
+        </button>
       </div>
-      <div className="text-center mt-2">
-        <button  className="btn btn-success"  onClick={() => dispatch({type:"increser2",val:5})}  > افزایش 2 </button>
-        <button className="btn btn-danger mr-3"   onClick={() => dispatch({type:"decreser2",val:3})}   >   کاهش 2  </button>
-      </div>
-      <button className="btn btn-info mr-3" onClick={() => dispatch({type:"resat"})}>
-        ریسیت
-      </button>
+     
     </div>
   );
 };
