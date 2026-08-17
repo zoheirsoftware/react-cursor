8636835a0fed53e23c68a04c5595e8a14d19b8fa
zoheirsoftware <zoheir.s@yahoo.com>
2026-08-17 16:30:35 +0330

    part-74

--- src/components/Registerform.jsx
+++ src/components/Registerform.jsx
@@ -1,79 +1,125 @@
-import React from 'react';
-import { ErrorMessage, FastField, Field, Form, Formik, useFormik } from 'formik'
-import * as Yub from 'yup'
-const   initialValues={
-    name:"zoheir",
-    email:'',
-    password:''
-}
-const  onSubmit=values=>{
-    console.log(values)
-}
-const  validate=values=>{
-    let errors={}
-    if(!values.name){
-        errors.name='لطفا نام را وارد کنید'
-    }
-    if(!values.email){
-        errors.email='لطفا ایمیل را وارد کنید'
-    }else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
-        errors.email = "لطفا قالب ایمیل را رعایت کنید مثال : aaa@example.bbb"
-    }
-    if(!values.password){
-        errors.password='لطفا پسورد را وارد کنید'
-    }
-    return errors;
-}
-const validationSchema=Yub.object({
-    name:Yub.string().required('لطفا نام را وارد کنید'),
-    email:Yub.string().email("لطفا قالب ایمیل را رعایت کنید مثال : aaa@example.bbb").required('لطفا ایمیل را وارد کنید'),
-    password:Yub.string().required('لطفا پسورد را وارد کنید').min('حداقل 8 کاراکتر استفاده کن'),
-})
-  const Registerform = () => {
-//     const formik=useFormik({
-//         initialValues ,
-//         onSubmit ,
-//        // validate , 
-//        validationSchema
-//     })
-    
- 
-    return (
-       <Formik initialValues={initialValues}
-       onSubmit={onSubmit}
-       validationSchema={validationSchema}>
-         <div className='auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0'>
-            <div className="row w-100 justify-content-center align-items-center">
-                <div className='auth_box col-11 col-md-8 col-lg-6 col-xl-4 py-4 px-3'>
-                    <Form >
-                        <h1 className='text-center'>
-                            <i className='fas fa-user-plus text-primary'></i>
-                        </h1>
-                        <div className="mb-3">
-                            <label htmlFor="name" className="form-label">نام</label>
-                            <Field type="text" className="form-control" id="name" name='name'  />
-                            <ErrorMessage name='name' />
-                           
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="email" className="form-label">ایمیل</label>
-                            <FastField type="email" className="form-control" id="email" name='email'  />
-                            <ErrorMessage name='email' />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="password" className="form-label">رمز عبور</label>
-                            <FastField type="password" className="form-control" id="password" name='password'  />
-                            <ErrorMessage name='password' />
-                        </div>
-                        <div className='text-center w-100'>
-                            <button type="submit" className="btn btn-primary">ثبت نام</button>
-                        </div>
-                    </Form>
-                </div>
-            </div>
+import React from "react";
+import {
+  ErrorMessage,
+  FastField,
+  Field,
+  Form,
+  Formik,
+  useFormik,
+} from "formik";
+import * as Yub from "yup";
+import Personalfield from "./Personalfield";
+import Personalerror from "./Personalerror";
+const initialValues = {
+  name: "zoheir",
+  email: "",
+  password: "",
+};
+const onSubmit = (values) => {
+  console.log(values);
+};
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "لطفا نام را وارد کنید";
+  }
+  if (!values.email) {
+    errors.email = "لطفا ایمیل را وارد کنید";
+  } else if (
+    !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+  ) {
+    errors.email = "لطفا قالب ایمیل را رعایت کنید مثال : aaa@example.bbb";
+  }
+  if (!values.password) {
+    errors.password = "لطفا پسورد را وارد کنید";
+  }
+  return errors;
+};
+const validationSchema = Yub.object({
+  name: Yub.string().required("لطفا نام را وارد کنید"),
+  email: Yub.string()
+    .email("لطفا قالب ایمیل را رعایت کنید مثال : aaa@example.bbb")
+    .required("لطفا ایمیل را وارد کنید"),
+  password: Yub.string()
+    .required("لطفا پسورد را وارد کنید")
+    .min("حداقل 8 کاراکتر استفاده کن"),
+});
+const Registerform = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <div className="auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0">
+        <div className="row w-100 justify-content-center align-items-center">
+          <div className="auth_box col-11 col-md-8 col-lg-6 col-xl-4 py-4 px-3">
+            <Form>
+              <h1 className="text-center">
+                <i className="fas fa-user-plus text-primary"></i>
+              </h1>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  نام
+                </label>
+                <Field
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                />
+                <ErrorMessage name="name" component={Personalerror} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  ایمیل
+                </label>
+                <FastField
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="email"
+                />
+                <ErrorMessage name="email">
+                  {(error) => (
+                    <small className="d-block text-center text-danger">
+                      {error}
+                    </small>
+                  )}
+                </ErrorMessage>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  رمز عبور
+                </label>
+                <FastField name="password">
+                  {(props) => <Personalfield {...props} />}
+                </FastField>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="bio" className="form-label">
+                  بیو{" "}
+                </label>
+                <FastField
+                  type="text"
+                  className="form-control"
+                  id="bi"
+                  name="bio"
+                  component="textarea"
+                />
+              </div>
+              <div className="text-center w-100">
+                <button type="submit" className="btn btn-primary">
+                  ثبت نام
+                </button>
+              </div>
+            </Form>
+          </div>
         </div>
-       </Formik>
-    );
-}
+      </div>
+    </Formik>
+  );
+};
 
 export default Registerform;
